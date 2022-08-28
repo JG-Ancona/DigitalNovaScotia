5c271b30a723086de2c9f7caa052f7b6d1c50aaf
jg_ancona <jgabriel.ancona@gmail.com>
2022-08-28 18:28:48 -0300

    initial commit

---
+++ SQL/Queries/Aditional Functions.sql
@@ -0,0 +1,21 @@
+USE sakila
+
+-- Round numeric values
+SELECT	amount,
+		ROUND(amount,0) Amnt, ROUND(amount,3) Amnt1,
+		FLOOR(amount) FloorAmnt, CEILING(amount) CeilingAmnt
+FROM payment
+
+
+
+-- CONCAT Function for string
+SELECT CONCAT(first_name,'|', last_name) AS FullName
+FROM actor
+
+-- LEFT Fun for strings
+SELECT	CONCAT(first_name, ' ', last_name) AS FullName,
+		CONCAT(LEFT(first_name,1), ' ', LEFT(last_name,1)) AS FirstInitial
+FROM actor
+
+--- LENGTH fun 
+
