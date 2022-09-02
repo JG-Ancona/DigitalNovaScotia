88d553e309e70a1c613b7c15d59b1002f62d292a
jg_ancona <jgabriel.ancona@gmail.com>
2022-09-02 19:21:34 -0300

    Left outer join

--- SQL/Queries/Joins-Unions-Subqueries.sql
+++ SQL/Queries/Joins-Unions-Subqueries.sql
@@ -21,6 +21,12 @@ CREATE TABLE Table2
 (ID INT, Value VARCHAR(10));
 
 INSERT INTO Table2 (ID, Value)
+	SELECT 1, 'First'
+	UNION ALL
+	SELECT 2, 'Second'
+	UNION ALL
+	SELECT 3, 'Third'
+	UNION ALL
 	SELECT 6, 'Sixth'
 	UNION ALL
 	SELECT 7, 'Seventh'
@@ -37,3 +43,22 @@ SELECT *
 FROM Table2;
 
 -- INNER JOIN
+
+SELECT t1.*,t2.*
+FROM Table1 t1
+INNER JOIN Table2 t2 ON t1.ID = t2.ID
+
+-- INNER JOIN with ALIAS
+
+SELECT	t1.ID AS T1ID, t1.Value AS T1Value,
+		t2.ID T2ID, t2.Value AS T2Value
+FROM Table1 t1
+INNER JOIN Table2 t2 ON t1.ID = t2.ID;
+
+
+-- LEFT OUTER JOIN
+
+SELECT	t1.ID AS T1ID, t1.Value AS T1Value,
+		t2.ID T2ID, t2.Value AS T2Value
+FROM Table1 t1
+LEFT JOIN Table2 t2 ON t1.ID = t2.ID;
