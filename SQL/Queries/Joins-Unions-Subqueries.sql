e4fe76099da6faecbdd81763735c9ff153889095
jg_ancona <jgabriel.ancona@gmail.com>
2022-09-03 13:31:51 -0300

    subqueries correlated

--- SQL/Queries/Joins-Unions-Subqueries.sql
+++ SQL/Queries/Joins-Unions-Subqueries.sql
@@ -141,3 +141,28 @@ INNER JOIN film_category fc ON fc.film_id = fl.film_id
 INNER JOIN category cat ON cat.category_id = fc.category_id
 WHERE cat.name = 'Action'
 ORDER BY cust.customer_id, cust.first_name, cust.last_name;
+
+
+--Subquery with inner join
+SELECT fm.title, cat.name, dt.CountofCategory
+FROM film fm
+INNER JOIN film_category fc ON fc.film_id = fm.film_id
+INNER JOIN category cat ON cat.category_id = fc.category_id
+INNER JOIN
+	(
+	SELECT COUNT(*) AS CountofCategory, fc.category_id
+	FROM film_category fc
+	GROUP BY fc.category_id
+	) dt ON dt.category_id = fc.category_id;
+
+-- Subquery with aggregate function and conditional
+SELECT payment_id, cust.first_name, cust.last_name, amount
+FROM payment pt
+INNER JOIN customer cust ON cust.customer_id = pt.customer_id
+WHERE amount >
+	(
+	SELECT AVG(amount)
+	FROM payment pt1
+	WHERE pt1.customer_id = pt.customer_id
+	)
+ORDER BY cust.customer_id;
