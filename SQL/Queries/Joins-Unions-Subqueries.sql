2ec2da81133912668c63793eec6d50832afa0b13
jg_ancona <jgabriel.ancona@gmail.com>
2022-09-03 13:11:18 -0300

    subqueries examples

--- SQL/Queries/Joins-Unions-Subqueries.sql
+++ SQL/Queries/Joins-Unions-Subqueries.sql
@@ -89,4 +89,55 @@ SELECT	t1.ID AS T1ID, t1.Value AS T1Value,
 FROM Table1 t1
 RIGHT OUTER JOIN Table2 t2 ON t1.ID = t2.ID;
 
+-- CROSS JOIN
 
+SELECT	t1.ID AS T1ID, t1.Value AS T1Value,
+		t2.ID T2ID, t2.Value AS T2Value
+FROM Table1 t1
+CROSS JOIN Table2 t2;
+
+--- Subquery 
+
+USE sakila;
+
+SELECT cust.customer_id, cust.first_name, cust.last_name
+FROM customer cust
+WHERE cust.customer_id IN
+	(
+	SELECT ren.customer_id
+	FROM rental ren
+	WHERE ren.inventory_id IN
+		(
+		SELECT inv.inventory_id
+		FROM inventory inv
+		WHERE inv.film_id IN
+			(
+			SELECT fl.film_id
+			FROM film fl
+			WHERE fl.film_id IN
+				(
+				SELECT fc.film_id
+				FROM film_category fc
+				WHERE fc.category_id IN
+					(
+					SELECT cat.category_id
+					FROM category cat
+					WHERE cat.name = 'Action'
+					)
+				)
+			)
+		)
+	)
+ORDER BY cust.customer_id, cust.first_name, cust.last_name;
+
+-- subquery with joins
+
+SELECT DISTINCT cust.customer_id, cust.first_name, cust.last_name
+FROM customer cust
+INNER JOIN rental ren ON ren.customer_id = cust.customer_id
+INNER JOIN inventory inv ON inv.inventory_id = ren.inventory_id
+INNER JOIN film fl ON fl.film_id = inv.film_id
+INNER JOIN film_category fc ON fc.film_id = fl.film_id
+INNER JOIN category cat ON cat.category_id = fc.category_id
+WHERE cat.name = 'Action'
+ORDER BY cust.customer_id, cust.first_name, cust.last_name;
