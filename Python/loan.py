cf804388b43c04c0c60937bb4f028c2bc12e0e37
jg_ancona <jgabriel.ancona@gmail.com>
2022-10-12 10:45:09 -0300

    calculator logic

--- Python/loan.py
+++ Python/loan.py
@@ -1,1 +1,23 @@
 # Calculates how much of your loan have been paid
+
+# Get the loan details
+
+money_owned = float(input("How much do you owe, in dollars?\n"))    # $50,000
+apr = float(input("What is the annual percentage rate?\n"))   # 3%
+payment = float(input("What will your monthly payment be, in dollars?\n"))
+months = int(input("How many months do you want to see results for?\n"))
+
+# Divide apr by 100 to make it a percent, then divide by 12 to make monthly
+monthly_rate = apr/100/12
+
+# Add interes
+
+interest_paid = money_owned * monthly_rate
+money_owned = money_owned + interest_paid
+
+# Make a payment
+money_owned = money_owned - payment
+
+# Print the result after this month
+print("Paid", payment, "of which", interest_paid, "was interes", end=' ')
+print("Now I owe", money_owned)
