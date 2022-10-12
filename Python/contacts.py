ce8998e104746b3b9d05bc11706fa26d75331390
jg_ancona <jgabriel.ancona@gmail.com>
2022-10-12 16:28:15 -0300

    initial commit

---
+++ Python/contacts.py
@@ -0,0 +1,16 @@
+from operator import contains
+
+
+contacts = {
+    "number":4,
+    "students":
+    [
+        {"name":"Sarah Holderness","email":"sarah@example.com"},
+        {"name":"Harry Potter","email":"harry@example.com"},
+        {"name":"Hermione Granger","email":"hermione@example.com"},
+        {"name":"Ron Weasly","email":"ron@example.com"},
+    ]
+}
+
+for student in contains["students"]:
+    print(student)
