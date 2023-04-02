722b28699b3db4fe76464071036459d309eac47c
jg_ancona <jgabriel.ancona@gmail.com>
2023-04-02 15:15:14 -0300

    added methods

--- Web Development/php content/class_objects.php
+++ Web Development/php content/class_objects.php
@@ -2,17 +2,54 @@
 
 /// Class definition
 
+/**
+ * Summary of Person
+ */
 class Person
 {
-    public $firstName = "Samuel Langhorne";
-    public $lastName = "Clemens";
-    public $yearBorn = 1899;
+    const AVG_LIFE_SPAN = 79;
+    public $firstName;
+    public $lastName;
+    public $yearBorn;
+
+    // function __construct()
+    // {
+    //     $firstName = "Samuel Langhorne";
+    //     $lastName = "Clemens";
+    //     $yearBorn = 1899;
+    // }
+
+    function __construct($tempFirst = "",$tempLast = "",$tempBorn = "")
+    {
+        $this->firstName = $tempFirst;
+        $this->lastName = $tempLast;
+        $this->yearBorn = $tempBorn;
+    }
+
+    public function getFirtsName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirtsName($tempName)
+    {
+        $this->firstName = $tempName;
+
+    }
+
 }
 
-$myObject = new Person();
+$myObject = new Person("Samuel","Adams",1899);
 
+/*
 echo $myObject -> firstName;
 echo "\n";
 
 $myObject->firstName = "L.S";
 echo $myObject -> firstName;
+
+
+echo $myObject->setFirtsName("Sam");
+*/
+echo "\n";
+echo $myObject->getFirtsName();
