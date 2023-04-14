aa2dda5a1dc6612ad6c8f2c0933f50c041c21973
jg_ancona <jgabriel.ancona@gmail.com>
2023-04-14 17:35:45 -0300

    Change directory

---
+++ Web Development/php content/operators_structures.php
@@ -0,0 +1,34 @@
+<?php
+
+// Operators
+
+/*
+    //sum
+    echo 8+3;
+    //rest
+    echo 8-3;
+    //product
+    echo 8*3;
+    //division
+    echo 8/3;
+    //modulus
+    echo 8%3;
+    //Exponentiation
+    echo 4**4;
+    */
+//
+// Increment dectement
+
+$alterByOne = 2;
+
+$alterByOne++;
+
+echo $alterByOne."\n";
+
+// Increment dectement
+
+$alterByOne = 2;
+
+$alterByOne--;
+
+echo $alterByOne;
