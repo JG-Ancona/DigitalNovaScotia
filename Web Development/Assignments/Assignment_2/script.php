5b44e1b1a4372508746268dc468fafb1a356dbed
jg_ancona <jgabriel.ancona@gmail.com>
2023-04-16 17:05:46 -0300

    added switch cases

--- Web Development/Assignments/Assignment_2/script.php
+++ Web Development/Assignments/Assignment_2/script.php
@@ -3,33 +3,38 @@ session_start();
 
 $_SESSION['Form_user'] = $_POST;
 
+if($_POST["First_name"] != "" && $_POST["Last_name"] != ""){
+    
+    if (isset($_POST['sub'])) {
+        $Rol = $_POST['Rol'];
+        //echo $Rol;
 
-if (isset($_POST['sub'])) {
-    $Rol = $_POST['Rol'];
-    //echo $Rol;
+        switch ($Rol) {
+            case 'Admin':
+                header("Location:admin_page.php");
 
-    switch ($Rol) {
-        case 'Admin':
-            header("Location:admin_page.php");
-
-            break;
-
-
-        case 'Manager':
-            header("Location:admin_page.php");
-
-            break;
+                break;
 
 
-        case 'CEO':
-            header("Location:ceo_page.php");
+            case 'Manager':
+                header("Location:admin_page.php");
 
-            break;
+                break;
 
-        default:
-            # code...
-            break;
+
+            case 'CEO':
+                header("Location:ceo_page.php");
+
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
     }
-
+}
+else{
+    header("Location:login_page.php");
 }
 ?>
