e460476a73e6c5e5d7a1e7a77c28086d22cd7f60
jg_ancona <jgabriel.ancona@gmail.com>
2023-05-21 11:58:24 -0600

    first commit

---
+++ Web Development/Assignments/Assignment_3/succesful_push.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halifax Canoe and Kayak</title>
+    <link rel="stylesheet" href="./../Assignment_1/style.css">
+    <link rel="stylesheet" href="./login.css">
+    <link rel="icon" type="image/png"
+        href="https://raw.githubusercontent.com/Zulinov/skillsProjects/main/paddle-white.png">
+</head>
+
+<body>
+
+    <header class="header_bar"> <!-- Navigation menu -->
+
+        <img id="nav_button" class="header_button"
+            src="https://raw.githubusercontent.com/Zulinov/skillsProjects/main/hamburger.png" alt="Menu">
+        <nav class="header_nav">
+            <ul class="nav_menu">
+                <li><a href="./../Assignment_1/index.php" >Home</li>
+                <li>Book a trip</li>
+                <li><a href="./../Assignment_2/admin_login.html">Admin login</a></li>
+
+            </ul>
+        </nav>
+
+
+    </header>
+    <main>
+
+        <div class="form_section">
+            <p class="form_header">
+                Adventure added succesfully!
+            </p>
+        </div>
+        <div id="link_home">
+            <a href="./../Assignment_1/index.php"> Review all adventures </a>        
+        </div>
+
+
+    </main>
+    <!-- Call to scripts sources -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="./../Assignment_1/script.js"></script>
+</body>
+
+</html>
